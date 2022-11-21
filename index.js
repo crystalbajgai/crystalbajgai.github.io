be3694fdeaf8dc99827fbb2db43e0805764865d3
Crissy <66400444+crystalbajgai@users.noreply.github.com>
2022-11-21 15:15:14 +0545

    Change in Site

---
+++ index.js
@@ -0,0 +1,84 @@
+function showabout(){
+    $("#about_container").css("display","inherit");
+    $("#about_container").addClass("animated slideInLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideInLeft");
+    },800);
+}
+function closeabout(){
+    $("#about_container").addClass("animated slideOutLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideOutLeft");
+        $("#about_container").css("display","none");
+    },800);
+}
+function showwork(){
+    $("#work_container").css("display","inherit");
+    $("#work_container").addClass("animated slideInRight");
+    setTimeout(function(){
+        $("#work_container").removeClass("animated slideInRight");
+    },800);
+}
+function closework(){
+    $("#work_container").addClass("animated slideOutRight");
+    setTimeout(function(){
+        $("#work_container").removeClass("animated slideOutRight");
+        $("#work_container").css("display","none");
+    },800);
+}
+function showcontact(){
+    $("#contact_container").css("display","inherit");
+    $("#contact_container").addClass("animated slideInUp");
+    setTimeout(function(){
+        $("#contact_container").removeClass("animated slideInUp");
+    },800);
+}
+function closecontact(){
+    $("#contact_container").addClass("animated slideOutDown");
+    setTimeout(function(){
+        $("#contact_container").removeClass("animated slideOutDown");
+        $("#contact_container").css("display","none");
+    },800);
+}
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+      $("#box").css("display","none");
+      $("#about").removeClass("animated fadeIn");
+      $("#contact").removeClass("animated fadeIn");
+      $("#work").removeClass("animated fadeIn");
+    },1000);
+},1500);
+
+var form = document.getElementById("form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "You form has been submitted. Thanks!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
