823fb475c64167c461e8b8f22d63bb3f2aecb4d1
Crissy <66400444+crystalbajgai@users.noreply.github.com>
2022-12-15 22:12:26 +0545

    Update particles.js

--- particles.js
+++ particles.js
@@ -33,7 +33,7 @@ var pJS = function(tag_id, params){
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#FFFAFA'
+          color: '#fffafa'
         },
         polygon: {
           nb_sides: 5
